d4b53668972c6cb417af5d85b3103a5432bf89b3
HamadaRen <hamada@two-six.jp>
2025-04-10 20:50:26 +0900

    fix/ メイン画面のヘッダー、サイドバーの修正

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -8,6 +8,7 @@ import { useState } from 'react';
 import { homedir } from 'os';
 import CustomerListPage from '../components/customer/CustomerListPage';
 import { SidebarItemsType } from '../types/application';
+import { SidebarHeader } from '../styles/application';
 
 export const SideBar = () => {
   const [sidebarItems, setSidebarItems] = useState<SidebarItemsType>('customer');
@@ -21,15 +22,34 @@ export const SideBar = () => {
 
   return (
     <StyledSideBar $animation={sideBarAnimationType}>
-      <StyledRow $selected={location.pathname === '/'} as={Link} to="/">
-        顧客マスタ
+      <SidebarHeader>基本機能</SidebarHeader>
+      <StyledRow $selected={location.pathname === '/'} as={Link} to="/" style={{ marginBottom: '3.75rem' }}>
+        <img src="/svg/icon_people.svg" alt="" style={{ width: '1.25rem', height: '1.25rem' }} />
+        <p>顧客管理</p>
       </StyledRow>
-      <StyledRow $selected={location.pathname === '/treatmentMenu'} as={Link} to="/treatmentMenu">
+      {/* <StyledRow $selected={location.pathname === '/treatmentMenu'} as={Link} to="/treatmentMenu">
         施術マスタ
-      </StyledRow>
-      <StyledRow $selected={location.pathname === '/sales'} as={Link} to="/sales">
+      </StyledRow> */}
+      {/* <StyledRow $selected={location.pathname === '/sales'} as={Link} to="/sales" style={{ marginBottom: '3.75rem' }}>
         売上マスタ
-      </StyledRow>
+      </StyledRow> */}
+      <SidebarHeader>管理者機能</SidebarHeader>
+      <StyledRowManagement>
+        <img src="/svg/icon_money.svg" alt="" style={{ width: '1.25rem', height: '1.25rem' }} />
+        <p>売り上げ管理</p>
+      </StyledRowManagement>
+      <StyledRowManagement>
+        <img src="/svg/icon_graph.svg" alt="" style={{ width: '1.25rem', height: '1.25rem' }} />
+        <p>分析</p>
+      </StyledRowManagement>
+      <StyledRowManagement>
+        <img src="/svg/icon_file.svg" alt="" style={{ width: '1.25rem', height: '1.25rem' }} />
+        <p>メニュー料金マスタ</p>
+      </StyledRowManagement>
+      <StyledRowManagement>
+        <img src="/svg/icon_ticket.svg" alt="" style={{ width: '1.25rem', height: '1.25rem' }} />
+        <p>チケット管理マスタ</p>
+      </StyledRowManagement>
     </StyledSideBar>
   );
 };
@@ -53,26 +73,53 @@ const StyledSideBar = styled.div<{ $animation: 'open' | 'close' }>`
       width: 0;
     }
   }
-  width: 0;
   animation: ${({ $animation }) => ($animation === 'open' ? 'open' : 'close')} 0.3s forwards;
   white-space: nowrap;
   overflow: hidden;
-  background-color: ${COLORS.DEEP_BLUE};
+  background-color: ${COLORS.MAIN_SIDEBAR_BROWN};
   color: white;
   display: flex;
+  width: 17.75rem;
+  height: 64.375rem;
+  padding-top: 0.9375rem;
   flex-direction: column;
-  height: 100%;
+  align-items: center;
+  gap: 0.5rem;
+  flex-shrink: 0;
 `;
 
 const StyledRow = styled.div<{ $selected?: boolean }>`
-  text-decoration: none;
-  color: inherit;
   display: flex;
+  height: 1.5rem;
+  padding: 0.5rem 0.625rem 0.5rem 1.875rem;
   align-items: center;
-  padding: 0.8rem 1rem;
-  cursor: pointer;
-  border-bottom: 1px solid #fafafa;
-  background-color: ${({ $selected }) => ($selected ? '#526A8E' : 'transparent')};
+  gap: 0.5rem;
+  align-self: stretch;
+  text-decoration: dashed;
+  font-size: 1.125rem;
+  font-weight: 900;
+  background-color: ${({ $selected }) => ($selected ? '#393b2e' : 'transparent')};
+  color: ${({ $selected }) => ($selected ? '#c8d5ad' : '#DCCEC1')};
+  border-left: ${({ $selected }) => ($selected ? '0.375rem solid #c8d5ad' : '0.375rem solid #393b2e')};
+
+  &:hover {
+    background-color: #393b2e;
+  }
+`;
+
+const StyledRowManagement = styled.div<{ $selected?: boolean }>`
+  display: flex;
+  height: 2.4rem;
+  padding: 0 0.625rem 0 1.875rem;
+  align-items: center;
+  gap: 0.5rem;
+  align-self: stretch;
+  color: #dccec1;
+  font-size: 0.9rem;
+  font-weight: 500;
+  line-height: normal;
+  background-color: ${({ $selected }) => ($selected ? '#454137' : 'transparent')};
+
   &:hover {
     background-color: #526a8e;
   }
