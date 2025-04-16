4e118fa5b73a9def3579e8c93aa524c3e7a56dbd
HamadaRen <hamada@two-six.jp>
2025-04-16 20:07:13 +0900

    fix/ 顧客情報モーダルのポジション修正

--- src/components/modal/CustomerDetailModal.tsx
+++ src/components/modal/CustomerDetailModal.tsx
@@ -10,39 +10,41 @@ import CustomerDetailConsent from './CustomerDetailConsent';
 import CustomerDetailTicket from './CustomerDetailTicket';
 import CustomerDetailModalFooter from '../footer/CustomerDetailModalFooter';
 
+type CustomerDetailModalType = {
+  showModal: boolean;
+};
 
-const CustomerDetailModal = () => {
+const CustomerDetailModal = ({ showModal }: CustomerDetailModalType) => {
   const [selected, setSelected] = useState<'customerInformation' | 'questionnaire' | 'history' | 'consent' | 'ticket'>(
     'customerInformation'
   );
 
-  let selectedData;
-  if(selected === 'customerInformation'){
-    selectedData = <CustomerDetailInformation />
-  }
-  else if(selected === 'questionnaire'){
-    selectedData = <CustomerDetailQuestionnaire />
-  }
-  else if(selected === 'history'){
-    selectedData = <CustomerDetailHistory />
-  }
-  else if(selected === 'consent'){
-    selectedData = <CustomerDetailConsent />
-  }
-  else if(selected === 'ticket'){
-    selectedData = <CustomerDetailTicket />
+  if (!showModal) {
+    return <></>;
   }
 
+  let selectedData;
+  if (selected === 'customerInformation') {
+    selectedData = <CustomerDetailInformation />;
+  } else if (selected === 'questionnaire') {
+    selectedData = <CustomerDetailQuestionnaire />;
+  } else if (selected === 'history') {
+    selectedData = <CustomerDetailHistory />;
+  } else if (selected === 'consent') {
+    selectedData = <CustomerDetailConsent />;
+  } else if (selected === 'ticket') {
+    selectedData = <CustomerDetailTicket />;
+  }
 
   return (
     <>
       <ModalContainer>
-          <CustomerDetailHeader />
-          <CustomerInformationContainer>
-            <CustomerDetailTab selected={selected} setSelected={setSelected} />
-            <div>{selectedData}</div>
-          </CustomerInformationContainer>
-          <CustomerDetailModalFooter />
+        <CustomerDetailHeader />
+        <CustomerInformationContainer>
+          <CustomerDetailTab selected={selected} setSelected={setSelected} />
+          <div>{selectedData}</div>
+        </CustomerInformationContainer>
+        <CustomerDetailModalFooter />
       </ModalContainer>
     </>
   );
